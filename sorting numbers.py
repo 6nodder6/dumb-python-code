e3d2fb734512a8c01495f1c6c5a96214d3b5cfe2
6nodder6 <39312536+6nodder6@users.noreply.github.com>
2021-10-10 18:08:45 -0700

    Add files via upload

---
+++ sorting numbers.py
@@ -0,0 +1,30 @@
+import time
+import random
+finished = 1
+number_list = []
+while finished == 1:
+    total_numbers = input("How many numbers do you want to sort? \n")
+    numberrange_min = int(input("What is the lowest numbmer allowed? \n"))
+    numberrange_max = int(input("what is the highest number allowed? \n"))
+    total_numbers = int(total_numbers)
+    progress = 0
+    while progress < total_numbers:
+        random_number = random.randint(numberrange_min, numberrange_max)
+        number_list.append(random_number)
+        progress = progress + 1
+    print("Your random number set is: ", number_list)
+    time.sleep(.5)
+    #considered using sorted() but it doesn't fit the constraints of the problem
+    x = 0
+    highest_number = (number_list[1])
+    while int(x) < int(total_numbers):
+        current_val = int(number_list[x])
+        print("The current number is: ", current_val)
+        x = x+1
+        if int(current_val) > int(highest_number):
+            highest_number = current_val
+            print("The current highest number is ", highest_number)
+    finished = 0
+print("THE HIGHEST NUMBER HAS BEEN SELECTED!")
+time.sleep(1)        
+print("\nthe highest number is: ", highest_number)
